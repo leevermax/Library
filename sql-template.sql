46a986b5279dbc49b7300c518e875a6c438de5e6
TIP Ocho <40892144+leevermax@users.noreply.github.com>
2020-06-25 17:01:01 +0700

    SQL  Case When...........

--- sql-template.sql
+++ sql-template.sql
@@ -6,3 +6,54 @@
 							+ "AND (m.BIRTHDAY <= ? OR (m.BIRTHDAY = m.BIRTHDAY AND ? = '')) " //where BIRTHDAY	TO  	| 2?
 							+ "AND m.DELETE_YMD IS NULL "
 							+ "ORDER BY m.CUSTOMER_ID";
+							
+// SQL  Case When...........
+public function FindProduct($key,$page){
+        $key = trim($key);
+    	$list = $this->dbGetlist("SELECT * FROM product 
+    								WHERE 
+                                        name LIKE ( CASE
+                                            WHEN
+                                                '$key' != ''
+                                                THEN  '%$key%'
+                                            ELSE  
+                                                'Giày Thể Thao XSPORT Ni.ke Air Force 1 Full Trắng SF'
+                                            END) 
+                                        OR description LIKE ( CASE
+                                                                WHEN
+                                                                    '$key' != ''
+                                                                    THEN  '%$key%'
+                                                                ELSE  
+                                                                'Giày Thể Thao XSPORT Ni.ke Air Force 1 Full Trắng SF'
+                                                                END)
+                                        OR product.category 
+                                            IN (SELECT category_level2.id 
+                                                FROM category_level2 
+                                                    WHERE category_level2.categoryLevel1 
+                                                        IN (SELECT category_level1.id 
+                                                                FROM category_level1 
+                                                                    WHERE category_level1.name
+                                                                        LIKE ( CASE
+                                                                                    WHEN
+                                                                                        '$key' != ''
+                                                                                        THEN  '%$key%'
+                                                                                    ELSE  
+                                                                                    'Giày Thể Thao XSPORT Ni.ke Air Force 1 Full Trắng SF'
+                                                                                    END)))
+                                        OR product.category 
+                                            IN (SELECT category_level2.id
+                                                    FROM category_level2
+                                                        WHERE category_level2.name 
+                                                            LIKE ( CASE
+                                                                    WHEN
+                                                                        '$key' != ''
+                                                                        THEN  '%$key%'
+                                                                    ELSE  
+                                                                    'Giày Thể Thao XSPORT Ni.ke Air Force 1 Full Trắng SF'
+                                                                    END))                                           
+                                        AND status != '0'
+                                        ORDER BY price
+                                        LIMIT $page, 20							
+    							");
+    	return $list;
+    }
