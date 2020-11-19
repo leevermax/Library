2be84176c68df4b0ef96fc07359f4e30f1759bde
TIP Ocho <40892144+leevermax@users.noreply.github.com>
2020-11-19 18:05:23 +0700

    Update GetContentAfter.js

--- GetContentAfter.js
+++ GetContentAfter.js
@@ -1,2 +1,4 @@
 // get content value of ::aftert tag of element....
 window.getComputedStyle(document.getElementById('myH1'), '::after').getPropertyValue('content');
+//elememt by class
+window.getComputedStyle(document.getElementsByClassName('khigio')[0], '::after').getPropertyValue('content')
